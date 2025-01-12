7cf13de225da61ce31acdb5a03f94f80de197d09
Ali Hosseini <a.hosseini9@outlook.com>
2025-01-12 14:30:05 +0330

    pagination added

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,24 +1,28 @@
 import { useEffect, useState } from "react";
 import Table from "../modules/Table";
 import { getApi } from "../../services/api";
+import Pagination from "../modules/Pagination";
 
 function HomePage() {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchData = async () => {
-      const response = await fetch(getApi());
+      const response = await fetch(getApi(page));
       const result = await response.json();
       setData(result);
       setIsLoading(false);
     };
     fetchData();
-  }, []);
+  }, [page]);
 
   return (
     <>
       <Table data={data} isLoading={isLoading} />
+      <Pagination page={page} setPage={setPage} />
     </>
   );
 }
